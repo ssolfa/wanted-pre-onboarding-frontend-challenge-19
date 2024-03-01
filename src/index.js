a0f1b2c4634e7d56b259f3c1422522dd667b98f3
solmee <kii5541@naver.com>
2024-03-01 20:24:20 +0900

    [FIX] react-dom/client 오류 해결

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import reducer from '../src/store/reducer';
 import { legacy_createStore as createStore } from 'redux';
 
 const store = createStore(reducer);
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
-ReactDOM.render(
+root.render(
     <Provider store={store}>
     <App />
     </Provider>
-    , document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+);
